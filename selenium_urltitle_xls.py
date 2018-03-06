154aad01ad4c9c66fc12f05efa4d81b53979cea1
githup-itcker <1284145843@qq.com>
2018-03-06 14:34:18 +0800

    Update selenium_urltitle_xls.py

--- selenium_urltitle_xls.py
+++ selenium_urltitle_xls.py
@@ -11,17 +11,17 @@ url_xls = []   #创建空列表
 title_xls = []
 
 for urls in file:
-	#options = webdriver.ChromeOptions()
-	#options.add_argument('--ignore-certificate-errors')
-	#browser = webdriver.Chrome(chrome_options = options)
-	cap = webdriver.DesiredCapabilities.PHANTOMJS
-	cap["phantomjs.page.settings.resourceTimeout"] = 200000
-	cap["phantomjs.page.settings.loadImages"] = True
-	cap["phantomjs.page.settings.disk-cache"] = True
-	cap["phantomjs.page.settings.userAgent"] = "Chrome/63.0.3239.108"
-	cap["phantomjs.page.customHeaders.User-Agent"] = "Chrome/63.0.3239.108"
-	cap["phantomjs.page.customHeaders.Referer"] = urls
-	browser = webdriver.PhantomJS(desired_capabilities=cap, service_args=['--ignore-ssl-errors=true'])   #Phantomjs中service_args参数可以忽略https错误
+	options = webdriver.ChromeOptions()
+	options.add_argument('--ignore-certificate-errors')
+	browser = webdriver.Chrome(chrome_options = options)
+	#cap = webdriver.DesiredCapabilities.PHANTOMJS
+	#cap["phantomjs.page.settings.resourceTimeout"] = 200000
+	#cap["phantomjs.page.settings.loadImages"] = True
+	#cap["phantomjs.page.settings.disk-cache"] = True
+	#cap["phantomjs.page.settings.userAgent"] = "Chrome/63.0.3239.108"
+	#cap["phantomjs.page.customHeaders.User-Agent"] = "Chrome/63.0.3239.108"
+	#cap["phantomjs.page.customHeaders.Referer"] = urls
+	#browser = webdriver.PhantomJS(desired_capabilities=cap, service_args=['--ignore-ssl-errors=true'])   #Phantomjs中service_args参数可以忽略https错误
 	browser.get(urls)
 	#time.sleep(2)
 
@@ -33,12 +33,12 @@ for urls in file:
 	url_xls.append(url)   #添加元素到列表中
 	title_xls.append(title)
 
-workbook = xlwt.Workbook(encoding = 'utf-8')   #创建工作簿	
+workbook = xlwt.Workbook(encoding='utf-8')   #创建工作簿	
 sheet = workbook.add_sheet('Sheet1',cell_overwrite_ok=True)   #创建sheet
 
 head = ['url', 'title']   # 表头部信息
 for row in range(0, len(head)):   #len列表长度
-	sheet.write(0, row, head[row], xlwt.easyxf('font: bold on'))   # 行，列
+	sheet.write(0, row, head[row], xlwt.easyxf('font:bold on'))   # 行，列
 
 for column1 in range(0, len(url_xls)):
 	sheet.write(column1 + 1, 0, url_xls[column1])
